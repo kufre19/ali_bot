5556d15474830b038d6b4c4f8874887be1bbc109
Emmanuel Kufre <whitemaxwell5@gmial.com>
2023-02-10 14:23:34 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::any('/bot',[\App\Http\Controllers\BotController::class,'test']);
+
+Route::any('/verify',[\App\Http\Controllers\BotController::class,'verify_bot']);
+
+
+
+// Route::any('/bot', function(Request $input)
+// {
+//     if (isset($input['hub_verify_token'])) { ## allows facebook verify that this is the right webhook
+//         if ($input['hub_verify_token'] ==="EmmaToken") {
+//             return $input['hub_challenge'];
+//             dd();
+//         } else {
+//             echo 'Invalid Verify Token';
+//             dd();
+//         }
+//     }
+// });
